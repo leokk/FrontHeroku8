cb027e45a6c9fb315b8154461243dbc0297dcc00
leokk <43683982+leokk@users.noreply.github.com>
2019-10-21 19:01:55 +0300

    changeDependencies

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
