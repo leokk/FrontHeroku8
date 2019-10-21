e24a86914f7d20c43f32e56d78a5c76adf812c44
leokk <43683982+leokk@users.noreply.github.com>
2019-10-21 19:41:10 +0300

    AnotherTry

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const path = require('path');
 
 const app = express();
 
-// Initialize the app.
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
